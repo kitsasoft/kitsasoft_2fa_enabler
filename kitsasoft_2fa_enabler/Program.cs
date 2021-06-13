f9ce6ad4908a2a675a2a1e61dd6b8ca7b63ed019
Astronomdef <78546167+Astronomdef@users.noreply.github.com>
2021-06-13 19:31:43 +0300

    Добавьте файлы проекта.

---
+++ kitsasoft_2fa_enabler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace kitsasoft_2fa_enabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enabling 2fa...");
+            Console.WriteLine("2fa has been enabled!");
+        }
+    }
+}
